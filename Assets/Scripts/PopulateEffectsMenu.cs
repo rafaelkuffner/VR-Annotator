8dc9e220b745e3115d5f6ee595e01e9136217ee1
Rafael Kuffner dos Anjos <rafaelkuffner@gmail.com>
2018-09-12 14:43:52 +0100

    Effects menu and 3 Sample visual effects
    Created so the annotation can be done later

---
+++ Assets/Scripts/PopulateEffectsMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class PopulateEffectsMenu : MonoBehaviour {
+
+    // Use this for initialization
+    string filesPath = "Assets\\Resources\\EffectPrefabs";
+    List<string> files;
+    void Start()
+    {
+        string[] f = System.IO.Directory.GetFiles(filesPath);
+        files = new List<string>(f);
+        GameObject butPrefab = Resources.Load("Prefabs/Button") as GameObject;
+        int count = 0;
+        foreach (string s in files)
+        {
+            if (s.Contains(".meta")) continue;
+            GameObject g = Instantiate(butPrefab);
+            g.name = s;
+            char[] sep = { '\\' };
+            string[] tokens = s.Split(sep);
+            string b = tokens[tokens.Length - 1];
+            b = b.Replace(".prefab", "");
+            g.GetComponentInChildren<Text>().text = b;
+            g.transform.SetParent(this.transform, false);
+            //g.GetComponentInChildren<Text>().text = b;
+            count++;
+            
+        }
+        if (count > 30)
+        {
+            GridLayoutGroup grid = GetComponent<GridLayoutGroup>();
+            grid.cellSize = new Vector2(grid.cellSize.x, grid.cellSize.y / 2.0f);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
